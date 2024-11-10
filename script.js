39a517f43f165be996cd9996acff6535b95ff375
DimaSt2 <svoy2@tut.by>
2024-11-10 20:06:09 +0300

    add: change main theme

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const phone = document.querySelector('.phone');
 
 const navLinks = document.querySelectorAll('.header-nav__links');
 const h1 = document.querySelector('h1');
-console.log('h1: ', h1);
 const testStrokeItems = document.querySelectorAll('.text-stroke');
 const btnRequest = document.querySelector('.btn-request');
 
@@ -15,29 +14,54 @@ const subMenuItems = document.querySelectorAll('.sub-menu__item');
 const subMenu = document.querySelector('.wrapper__sub-menu');
 const mainPromo = document.querySelector('.main-promo');
 
-
-const h1_addElem = '<span class="text-stroke">в Москве и Московской области</span>' 
-
-// logo.src = '/img/Logo__active.svg';
-// mapMarker.src = '/img/Map Marker__active.svg';
-// phone.src = '/img/Phone__active.svg';
-
-subMenuItems[0].addEventListener('click', () => {
+function changeTheme (index) {
     logo.src = '/img/Logo__active.svg';
     mapMarker.src = '/img/Map Marker__active.svg';
     phone.src = '/img/Phone__active.svg';
     subMenu.style.backgroundImage = 'url("")';
-    mainPromo.style.backgroundImage = 'url(./img/bg-menu1.png)';
     navLinks[0].classList.add('active');
     navLinks[1].classList.add('active');
     h1.classList.add('active');
-    h1.textContent = 'Услуги по ремонту';
-    testStrokeItems[0].classList.add('active');
     testStrokeItems[1].classList.add('active');
-    // testStrokeItems[1].textContent = 'в Москве и Московского области';
     btnRequest.classList.add('active');
+    switch(index) {
+        case 0:
+            mainPromo.style.backgroundImage = 'url(./img/bg-menu1.png)';
+            h1.textContent = 'Услуги по ремонту';
+            break;
+        case 1:
+            mainPromo.style.backgroundImage = 'url(./img/bg-menu2.png)';
+            h1.textContent = 'Виды работ по ремонту';
+            break;
+        case 2:
+            mainPromo.style.backgroundImage = 'url(./img/bg-menu3.png)';
+            h1.textContent = 'Дизайн интерьера';
+            break;
+        case 3:
+            mainPromo.style.backgroundImage = 'url(./img/bg-menu4.png)';
+            h1.textContent = 'Ремонт помещений';
+            break;
+    }
+}
 
-
-
-
+subMenuItems.forEach((elem, index) => {
+    elem.addEventListener('click', () => {
+        changeTheme(index)})
 })
+
+
+// subMenuItems[0].addEventListener('click', () => {
+//     logo.src = '/img/Logo__active.svg';
+//     mapMarker.src = '/img/Map Marker__active.svg';
+//     phone.src = '/img/Phone__active.svg';
+//     subMenu.style.backgroundImage = 'url("")';
+//     mainPromo.style.backgroundImage = 'url(./img/bg-menu1.png)';
+//     navLinks[0].classList.add('active');
+//     navLinks[1].classList.add('active');
+//     h1.classList.add('active');
+//     h1.textContent = 'Услуги по ремонту';
+//     testStrokeItems[0].classList.add('active');
+//     testStrokeItems[1].classList.add('active');
+//     btnRequest.classList.add('active');
+
+// })
